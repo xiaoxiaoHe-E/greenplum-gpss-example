6ea1d23eff0545ca53e3a618ece15cc72cfa18fa
xiaoxiaoHe-E <53000479+xiaoxiaoHe-E@users.noreply.github.com>
2019-09-18 17:06:36 +0800

    Update client.py

--- client.py
+++ client.py
@@ -5,13 +5,17 @@ import gpss_pb2_grpc
 import grpc
 import google.protobuf 
 import google.protobuf.text_format as tf
+import csv
 
 mSession = gpss_pb2.Session()
 gpMasterHost = "localhost"
-gpMasterPort = 8080         #port of gpss which is different from the port of gpdb
+gpMasterPort = 50007  # port of gpss which is different from the port of gpdb
 gpRoleName = "gpadmin"
 gpPasswd = "changeme"
 dbname = "testdb"
+
+csvPath = 'test.csv'
+csvHaveTitle = True
 
 def ConnectToGPSS():
     #connect to gpdb
@@ -20,7 +24,7 @@ def ConnectToGPSS():
     stub = gpss_pb2_grpc.GpssStub(channel)
     mConnectReq = gpss_pb2.ConnectRequest(
         Host = gpMasterHost,
-        Port = 15432,             #port of gpdb which is different from port of gpss
+        Port = 5432,             #port of gpdb which is different from port of gpss
         Username = gpRoleName,  
         Password = gpPasswd,
         DB = dbname,
@@ -64,36 +68,84 @@ def WritData(mSession, stub, schema):
                                    TableName="test",
                                    PreSQL="",
                                    PostSQL="",
-                                   Timeout=10,     # seconds
+                                   Timeout=10,       # seconds
                                    Encoding="UTF_8",
                                    StagingSchema= "",
-                                   Option=insOpt)
+                                   InsertOption=insOpt)
     stub.Open(openReq)
 
     #perpare dummy data to write
+    #table test (a text, b integer)
+    #need to specify all the columns in the table
     myRowData = []
     for i in range(5):
-        valB = data_pb2.DBValue(StringValue = str("aa"+str(i)))
-        valA = data_pb2.DBValue(Int64Value = 1)
+        valA = data_pb2.DBValue(StringValue = str('aa'+str(i)))
+        valB = data_pb2.DBValue(Int64Value = i)
         myRow = data_pb2.Row(Columns = [valA, valB])
-        print("original", myRow)
-        myRowinBytes = tf.MessageToBytes(myRow )
+        #print("original data: ", myRow)
+        myRowinBytes = myRow.SerializeToString()
         myRowData.append(gpss_pb2.RowData(Data=myRowinBytes))
 
     writeReq = gpss_pb2.WriteRequest(Session=mSession, Rows= myRowData)
     stub.Write(writeReq)
 
     #close the write service
-    closeReq = gpss_pb2.CloseRequest(session = mSession, 
-                                     CloseRequest = 5)
+    closeReq = gpss_pb2.CloseRequest(session=mSession,
+                                     MaxErrorRows = 5)
     state = stub.Close(closeReq)
-    print("response from server: ", state)
+    print("write response from server: ", state)
+
+
+def writFromCsv(mSession, stub, schema):
+    #write into gpdb from a csv file
+
+    #start an insert service
+    insOpt = gpss_pb2.InsertOption(
+        InsertColumns=["a", "b"],  # colum list to be inserted
+        TruncateTable=False,  # truncate the table before inserting or not
+        ErrorLimitCount=5,            #
+        ErrorLimitPercentage=5
+    )
+    openReq = gpss_pb2.OpenRequest(Session=mSession,
+                                   SchemaName=schema,
+                                   TableName="test",
+                                   PreSQL="",
+                                   PostSQL="",
+                                   Timeout=10,       # seconds
+                                   Encoding="UTF_8",
+                                   StagingSchema="",
+                                   InsertOption=insOpt)
+    stub.Open(openReq)
+
+    #read data from csv file
+    csvFile = open(csvPath, "r")
+    reader = csv.reader(csvFile)
+    myRowData = []
+    for item in reader:
+        if csvHaveTitle == True and reader.line_num == 1:  # skip the first line if has title
+            continue
+        valA = data_pb2.DBValue(StringValue=item[0])
+        valB = data_pb2.DBValue(Int64Value=int(item[1]))
+        myRow = data_pb2.Row(Columns=[valA, valB])
+        #print("original data: ", myRow)
+        myRowinBytes = myRow.SerializeToString()
+        myRowData.append(gpss_pb2.RowData(Data=myRowinBytes))
+
+    writeReq = gpss_pb2.WriteRequest(Session=mSession, Rows=myRowData)
+    stub.Write(writeReq)
+
+    #close the write service
+    closeReq = gpss_pb2.CloseRequest(session=mSession,
+                                     MaxErrorRows=5)
+    state = stub.Close(closeReq)
+    print("write response from server: ", state)
 
 
 if __name__ == '__main__':
     channel, mSession, stub = ConnectToGPSS()
     theSchema = ListSchema(mSession, stub)
     WritData(mSession, stub, theSchema)
+    writFromCsv(mSession, stub, theSchema)
 
     #close channel
     stub.Disconnect(mSession)
